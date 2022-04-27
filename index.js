348b7de3c04b3b53d1f85a6ef1c226a8037b6ae9
Pourab Karchaudhuri <pourab.karchaudhuri@gmail.com>
2022-04-27 21:09:24 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+var http = require('http');
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var fs = require('fs');
+
+var path = require('path')
+
+
+//Configuring the Express Middleware
+var app = express();
+
+// var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {flags: 'a'})
+
+//Configure Morgan's Logging Formats
+// app.use(morgan('common', {stream: accessLogStream}))    //UNCOMMENT TO ENABLE FILE LOGGING
+app.use(morgan('common'));
+
+
+//Set PORT to Dynamic Environments to run on any Server
+var port = process.env.PORT || 3001;
+
+//Configure Express to Recieve JSON and extended URLencoded formats
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.use(cors({
+    exposedHeaders : "*"
+}));
+
+app.use(bodyParser.json({
+    limit: "50mb"
+}))
+//Set RESTful routes
+
+app.get('/', function(req, res) {
+  res.status(200).json({
+      status: 'success',
+  })
+});
+//Route for GET
+
+
+//Route for file uploads
+
+app.listen(port);
+console.log("Server started successfully at PORT : " + port);
+//module.exports=app;
