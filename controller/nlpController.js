0f4e31bc0d044fb44a92b2e6d2adf55722df6b4f
mahuaSleepycat <mahua@sleepycat.in>
2022-04-27 22:21:24 +0530

    fixed the controller as required

--- controller/nlpController.js
+++ controller/nlpController.js
@@ -1,16 +1,24 @@
-const express = require("express");
+exports.sentimentAnalysisHandler = async (req, res) => {
 
-const router = express.Router();
-const {
-    sentimentAnalysisHandler,
-  } = require("../controller/nlpController.js");
-/**
- * Get API
- * @route GET /analysis
- * @group Sentiment Analysis API
- * @returns {object} 200 - Sentiment Analysis
- * @returns {Error}  default - Unexpected error
- */
- router.get("/analysis", sentimentAnalysisHandler);
-
- module.exports = router;
+  res.status(200).json({
+      status: 'success',
+      data: {
+          "analysis": {
+              "sentiment": {
+                  "score": 0.5,
+                  "magnitude": 0.5
+              },
+              "keywords": [
+                  {
+                      "text": "test",
+                      "relevance": 0.5,
+                      "sentiment": {
+                          "score": 0.5,
+                          "magnitude": 0.5
+                      }
+                  }
+              ]
+          }
+      }
+  });
+}
